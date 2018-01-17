692bb369517760ce87bb89b6786aefc55c931d75
Marko Bjelac <marko.bjelac@gmail.com>
2018-01-17 21:52:40 +0100

    added tests for map2 and map3

--- src/test/java/hr/mbjelac/sandbox/ascii_path_follower/acceptance/PathFollowingTest.java
+++ src/test/java/hr/mbjelac/sandbox/ascii_path_follower/acceptance/PathFollowingTest.java
@@ -21,6 +21,46 @@ public class PathFollowingTest {
         andLettersAre("ACB");
     }
 
+    @Test
+    public void map2() {
+
+        givenMap(
+                "@",
+                "| C----+",
+                "A |    |",
+                "+---B--+",
+                "  |      x",
+                "  |      |",
+                "  +---D--+"
+        );
+
+        whenFollowingPath();
+
+        thenPathIs("@|A+---B--+|+----C|-||+---D--+|x");
+        andLettersAre("ABCD");
+    }
+
+    @Test
+    public void map3() {
+
+        givenMap(
+                "  @---+",
+                "      B",
+                "K-----|--A",
+                "|     |  |",
+                "|  +--E  |",
+                "|  |     |",
+                "+--E--Ex C",
+                "   |     |",
+                "   +--F--+"
+        );
+
+        whenFollowingPath();
+
+        thenPathIs("@---+B||E--+|E|+--F--+|C|||A--|-----K|||+--E--Ex");
+        andLettersAre("BEEFCAKE");
+    }
+
     private void givenMap(String... asciiMapRows) {
 
     }
