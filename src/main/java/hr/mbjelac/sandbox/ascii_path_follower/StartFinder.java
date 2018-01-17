1da95858166d8bbf7f28d444a0de07542b94f335
Marko Bjelac <marko.bjelac@gmail.com>
2018-01-17 23:48:29 +0100

    implemented finding starting character coordinates

--- src/main/java/hr/mbjelac/sandbox/ascii_path_follower/StartFinder.java
+++ src/main/java/hr/mbjelac/sandbox/ascii_path_follower/StartFinder.java
@@ -1,7 +1,13 @@
 package hr.mbjelac.sandbox.ascii_path_follower;
 
 
+import lombok.val;
+
+import java.util.Arrays;
+
 class StartFinder {
+
+    private static final char STARTING_CHARACTER = '@';
 
     State findStart(AsciiMap map) {
 
@@ -9,6 +15,29 @@ class StartFinder {
             throw new IllegalArgumentException("map is null!");
         }
 
-        throw new IllegalArgumentException();
+        for (int rowIndex = 0; rowIndex < map.getRows().length; rowIndex++) {
+
+            val row = map.getRows()[rowIndex];
+
+            for (int columnIndex = 0; columnIndex < row.length; columnIndex++) {
+
+                val c = row[columnIndex];
+
+                if (c == STARTING_CHARACTER) {
+
+                    return State
+                            .builder()
+                            .x(columnIndex)
+                            .y(rowIndex)
+                            .build();
+                }
+            }
+        }
+
+        throw new IllegalArgumentException(
+                "Map missing starting character " +
+                        "'" + STARTING_CHARACTER + "': " +
+                        Arrays.deepToString(map.getRows()));
     }
+
 }
